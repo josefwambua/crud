d4ff251a1151f9754a7c38e3af3563653d315669
unknown <josef@seedArimathea.kim.ac.ke>
2025-05-28 10:39:45 +0300

    final

--- resources/views/update_post.blade.php
+++ resources/views/update_post.blade.php
@@ -1,24 +1,40 @@
 <x-app-layout>
-<div>
-    <form action="{{url('confirm_update', $data->id)}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label>Post</label>
-            <input type="text" name="description" value="{{$data->description}}">
-            <br>
+    <!-- Optional Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-            <label for="image">Curent Image</label>
-            <img src="/post/{{$data->image}}" height="300px" width="250" alt="">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow-sm">
+                    <div class="card-header bg-primary text-white fw-semibold">
+                        Update Post
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ url('confirm_update', $data->id) }}" method="POST" enctype="multipart/form-data">
+                            @csrf
 
-            <br>
-            <label for="Image">Change Image</label>
-            <input type="file" name="image" value="{{$data}}">
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Post Description</label>
+                                <input type="text" class="form-control" name="description" value="{{ $data->description }}" required>
+                            </div>
 
-            <br>
-            <div>
-                <input type="submit" value="updatePost">
+                            <div class="mb-3">
+                                <label class="form-label">Current Image</label><br>
+                                <img src="{{ asset('post/' . $data->image) }}" alt="Current Post Image" class="rounded border mb-2" width="100%" height="auto">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="image" class="form-label">Change Image</label>
+                                <input type="file" class="form-control" name="image">
+                            </div>
+
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-success">Update Post</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </form>
-</div>
+    </div>
 </x-app-layout>
